a7a249169cab252d060d9bd34e79aa0fe826f326
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-10-07 22:24:43 +1000

    Add default shaders

--- source/d2d/constants.d
+++ source/d2d/constants.d
@@ -18,5 +18,73 @@ static class Constants {
 		public enum maxProfilerSamples = 100;
 		public enum maxTextureRes = 4069;
 		public enum right = vec2(1f, 0f);
+
+		// TODO: Move to default shaders class?
+		public enum defaultSpritVertSrc =
+		`#version 410
+
+		layout(location = 0) in vec2 position;
+		layout(location = 1) in vec4 colour;
+		layout(location = 2) in vec2 uv;
+
+		out vec4 fragColour;
+		out vec2 fragUV;
+
+		uniform mat4 mvp;
+
+		void main() {
+			gl_Position = mvp * vec4(position, 0, 1);
+			fragColour = colour;
+			fragUV = uv;
+		}
+		`;
+
+		public enum defaultSpriteFragSrc =
+		`#version 410
+
+		in vec4 fragColour;
+		in vec2 fragUV;
+
+		uniform sampler2D spr;
+
+		out vec4 colour;
+
+		void main() {
+			colour = fragColour * texture(spr, fragUV);
+		}
+		`;
+
+		public enum defaultLightVertSrc =
+		`#version 410
+
+		layout(location = 0) in vec2 position;
+		layout(location = 1) in vec4 colour;
+		layout(location = 2) in vec2 uv;
+
+		out vec4 fragColour;
+		out vec2 fragUV;
+
+		uniform mat4 mvp;
+
+		void main() {
+			gl_Position = mvp * vec4(position, 0, 1);
+			fragColour = colour;
+			fragUV = uv;
+		}
+		`;
+
+		public enum defaultLightFragSrc =
+		`#version 410
+
+		in vec4 fragColour;
+		in vec2 fragUV;
+
+		out vec4 colour;
+
+		void main() {
+			float distance = length(fragUV);
+			colour = vec4(fragColour.rgb, fragColour.a * (1.0 - distance);
+		}
+		`;
 	}
 }
